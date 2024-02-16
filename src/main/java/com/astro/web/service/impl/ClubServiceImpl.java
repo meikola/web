f41c3423dda561217c9e46b3a67ba9d8b6c817cd
Meyrambek <m.zhusipp@gmail.com>
2024-02-16 17:36:35 +0600

    update всё более менее подключен. -Исправлено что изображение с ПостГре отображаються -Обновленный html -И так далее мелкие исправление

--- src/main/java/com/astro/web/service/impl/ClubServiceImpl.java
+++ src/main/java/com/astro/web/service/impl/ClubServiceImpl.java
@@ -4,14 +4,14 @@ import com.astro.web.dto.ClubDto;
 import com.astro.web.models.Club;
 import com.astro.web.repository.ClubRepository;
 import com.astro.web.service.ClubService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
 public class ClubServiceImpl implements ClubService {
-    private ClubRepository clubRepository;
+    private final ClubRepository clubRepository;
 
     public ClubServiceImpl(ClubRepository clubRepository) {
         this.clubRepository = clubRepository;
@@ -20,10 +20,11 @@ public class ClubServiceImpl implements ClubService {
     @Override
     public List<ClubDto> findAllClubs(){
         List<Club> clubs = clubRepository.findAll();
-        return clubs.stream().map((club) -> mapToClubDto(club)).collect(Collectors.toList());
+        return clubs.stream().map(this::mapToClubDto).collect(Collectors.toList());
     }
+
     private ClubDto mapToClubDto(Club club){
-        ClubDto clubDto = ClubDto.builder()
+        return ClubDto.builder()
                 .id(club.getId())
                 .title(club.getTitle())
                 .photoUrl(club.getPhotoUrl())
@@ -31,7 +32,5 @@ public class ClubServiceImpl implements ClubService {
                 .createdOn(club.getCreatedOn())
                 .updateOn(club.getUpdateOn())
                 .build();
-        return clubDto;
     }
-
 }
